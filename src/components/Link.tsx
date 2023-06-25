a382edf678a493f7643dad2e58c8e63e8be8cb1b
siwooJang <joy8417@naver.com>
2023-06-25 22:49:59 +0900

    Login added

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -9,10 +9,7 @@ export const Link: FC<LinkProps> = ({className: _className, to, ...props}) => {
   const resolved = useResolvedPath(to)
   // console.log('resolved', resolved)
   const match = useMatch({path: resolved.pathname, end: true})
-  //  console.log('match', match)
-  const className = [match && _className, match ? 'btn-outline' : ''].join(' ')
-
-  //console.log('match', resolved.pathname, className)
-
+  // console.log('match', match)
+  const className = [_className, match ? 'btn-active' : ''].join(' ')
   return <RRLink {...props} to={to} className={className} />
 }
--- src/routes/RoutesSetup.tsx
+++ src/routes/RoutesSetup.tsx
@@ -1,7 +1,11 @@
 import {Routes, Route} from 'react-router-dom'
 import Layout from './Layout'
+import RequireAuth from './Auth/RequireAuth'
 import LandingPage from './LandingPage'
 import Board from '../pages/Board'
+import Signup from './Auth/SignUp'
+import Login from './Auth/Login'
+import Logout from './Auth/Logout'
 import NoMatch from './NoMatch'
 
 export default function RoutesSetup() {
@@ -9,9 +13,27 @@ export default function RoutesSetup() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<LandingPage />} />
-        <Route path="/board" element={<Board />} />
+        <Route
+          path="/board"
+          element={
+            <RequireAuth>
+              <Board />
+            </RequireAuth>
+          }
+        />
         <Route path="*" element={<NoMatch />} />
       </Route>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/logout"
+        element={
+          <RequireAuth>
+            <Logout />
+          </RequireAuth>
+        }
+      />
+      <Route path="*" element={<NoMatch />} />
     </Routes>
   )
 }
